3b2bc9a541ed7c63bf88235bc57d444c3264b029
Advic <amazingadvicsaha443@gmail.com>
2025-08-28 14:15:10 +0530

    modified something

--- typingtest/database.py
+++ typingtest/database.py
@@ -18,15 +18,3 @@ def get_connection():
         )
 
     return conn
-
-# try:
-#     conn = mysql.connector.connect(
-#         host="127.0.0.1",
-#         port=3306,
-#         user="root",
-#         password="Advicsaha@443@443_443"  # hardcode temporarily to rule out .env issue
-#     )
-#     print("Connected?", conn.is_connected())
-# except Exception as e:
-#     print("Error:", e)
-#     traceback.print_exc()
