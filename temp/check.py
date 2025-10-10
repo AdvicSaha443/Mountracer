5e9315a037717cd43162f3f96cb114648d499abd
Advic <amazingadvicsaha443@gmail.com>
2025-10-10 17:17:30 +0530

    partially completed Table Class

--- temp/check.py
+++ temp/check.py
@@ -2,6 +2,9 @@ import shutil
 text = "She had never observed his face more composed and she grabbed his hand and held it to her heart. It was resistless and dry. The outline of a skull was plain under his skin and the deep burned eye sockets seemed to lead into the dark tunnel where he had disappeared. She leaned closer and closer to his face, looking deep into them, trying to see how she had been cheated or what had cheated her, but she couldn't see anything. She shut her eyes and saw the pin point of light but so far away that she could not hold it steady in her mind. She felt as if she were blocked at the entrance of something. She sat staring with her eyes shut, into his eyes, and felt as if she had finally got to the beginning of something she couldn't begin, and she saw him moving farther and farther away, farther and farther into the darkness until he was the pin point of light."
 
 class Table:
+    DEFAULT_WIDTH = ["inner_text", "terminal"]
+    OVERFLOW = ["hidden", "new_line"]
+    JUSTIFY = ["start", "end", "center"]
     THEMES = {
         "light": {
             "h": "─", "v": "│",
@@ -54,8 +57,114 @@ class Table:
     def get_theme(cls, name: str) -> str | None:
         return cls.THEMES.get(name)
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        title: str = None,
+        theme: str = "light"
+    ) -> None:
+        
+        self._title = title
+        self._theme = theme if theme in Table.THEMES else "light"
+        self._table = None
+
+        self._columns = []
+        self._rows = []
+        self.__maximum_column_width = []
+
+    def __str__(self):
+        self.__update_table()
+        return self._table
+    
+    @property
+    def columns(self) -> list:
+        return self._columns
+
+    @property
+    def rows(self) -> list:
+        return self._rows 
+    
+    def add_column(self, text: str = None, justify: str = "center", padding: tuple = (0, 0), rows_padding: tuple = (0, 0), overflow: str = "hidden") -> None:
+        self._columns.append((
+            text,
+            justify if justify in Table.JUSTIFY else "center",
+            padding if len(padding) == 2 else (0, 0),
+            rows_padding if len(rows_padding) == 2 else (0, 0),
+            overflow if overflow in Table.OVERFLOW else "hidden"
+        ))
+
+    def add_row(self, *values):
+        self._rows.append(list(values))
+
+    def __update_table(self) -> None:
+        title = self._title
+        theme = Table.THEMES[self._theme]
+
+        columns = [x[0] for x in self._columns]
+        columns_settings = self._columns
+        rows = self._rows
+
+        # checking the maximum number of row/header row and empty elements will be added accordingly.
+        # maximum_columns = max(len(max(columns, key = len)), len(max(rows, key = len)))
+        maximum_columns = max(len(columns), len(max(rows, key = len)))
+        table = []
+
+        table.append(columns) if len(columns) == maximum_columns else table.append(columns + [""*(maximum_columns-len(columns))] if (maximum_columns-len(columns)) != 0 else [])
+        for row in rows: table.append(row) if len(row) == maximum_columns else table.append(row + [""*(maximum_columns-len(row))] if (maximum_columns - len(row)) != 0 else [])
+
+        # calculating the maximum admissible length for each column
+        max_column_length = [max([len(str(x[i])) for x in table]) for i in range(0, len(table[0]))]
+        table_ = ""
+
+        table_width = sum(max_column_length) + 1 + len(table[0]) # constant values added for borders
+        terminal_width = shutil.get_terminal_size().columns
+
+        #check whether if table_width is greater than the terminal width
+        if terminal_width < table_width: print("Terminal width is greater than the table width!")
+
+        # building the table:
+        if title is not None:
+            if table_width%2 != 0:
+                if len(title)%2 != 0: title_position = ((table_width-1)/2) - (len(title)-1)/2
+                else: title_position = (table_width-1)/2 - len(title)/2
+            else:
+                if len(title)%2 == 0: title_position = (table_width/2) - len(title)/2
+                else: title_position = table_width/2 - (len(title)-1)/2
+
+            table_ += " "*(int(title_position)) + title + "\n"
+        
+        #table_ += theme["tl"] + ((theme["h"]*max_column_length[i] + theme["t"]) for i in range(0, len(max_column_length)))[:-1] + theme["tr"]
+        table_ += theme["tl"] + "".join((theme["h"]*max_column_length[i] + theme.get("t")) for i in range(0, len(max_column_length)))[:-1] + theme["tr"] + "\n"
+
+        # calculating justified position for each column:
+        justified_header = []
+        for i, elem in enumerate(table[0]):
+            if columns_settings[i][1] == "start": justified_header.append(elem + "".join(" " for _ in range(len(elem), max_column_length[i])))
+            elif columns_settings[i][1] == "end": justified_header.append("".join(" " for _ in range(0, max_column_length[i]-len(elem))) + elem)
+            elif columns_settings[i][1] == "center":
+                current_column_width = max_column_length[i]
+
+                if current_column_width%2 != 0:
+                    if len(elem)%2 != 0: title_position = ((current_column_width-1)/2) - (len(elem)-1)/2
+                    else: title_position = (current_column_width-1)/2 - len(elem)/2
+                else:
+                    if len(elem)%2 == 0: title_position = (current_column_width/2) - len(elem)/2
+                    else: title_position = current_column_width/2 - (len(elem)-1)/2
+
+                justified_header.append(" "*(int(title_position)) + elem + " "*(current_column_width - len(elem) - int(title_position)))
+        
+        table_ += theme["v"] + theme["v"].join(justified_header) + theme["v"] + "\n"
+        table_ += theme["l"] + "".join((theme["h"]*max_column_length[i] + theme.get("c")) for i in range(0, len(max_column_length)))[:-1] + theme["r"] + "\n"
+
+        for i in range(1, len(table)):
+            row = table[i]
+            table_ += "".join(theme["v"] + elem + "".join(" " for _ in range(len(elem), max_column_length[j])) for j, elem in enumerate(row)) + theme["v"] + "\n"
+
+        table_ += theme["bl"] + "".join((theme["h"]*max_column_length[i] + theme.get("b")) for i in range(0, len(max_column_length)))[:-1] + theme["br"]
+
+
+        #table_ += "".join(theme["v"] + elem + "".join(" " for _ in range(len(elem), max_column_length[i])) for i, elem in enumerate(table[0])) + theme["v"]
+
+        self._table = table_
 
 class Panel:
     THEMES = {
@@ -111,10 +220,10 @@ class Panel:
     def maximum_line_length() -> int:
         pass
 
-    def set_title(self, title: str = None, justify_title: str = "center", title_padding: tuple = (0, 0)) -> None:
+    def set_title(self, title: str = None, justify: str = "center", padding: tuple = (0, 0)) -> None:
         self._title = title
-        self._justify_title = justify_title if justify_title in Panel.JUSTIFY else "center"
-        self._title_padding = title_padding if len(title_padding) == 2 else (0, 0)
+        self._justify_title = justify if justify in Panel.JUSTIFY else "center"
+        self._title_padding = padding if len(padding) == 2 else (0, 0)
     
     def set_inner_text(self, inner_text: str = None, inner_text_padding: tuple = (0, 0, 0, 0), default_width: str = "inner_text", overflow: str = "hidden") -> None:
         self._inner_text = inner_text
@@ -225,36 +334,51 @@ class Panel:
 
         return "".join((word+ " ") for word in final_text_list)
 
-panel1 = Panel(
-    title=" Sample Title ",
-    justify_title="center",
-    title_padding=(0, 0),
-    inner_text_padding=(1, 0, 2, 1),
-    inner_text = "This is a sample line!\nThis is another sample line!\nThis is reallllllllyyyyyyyyyyyyyyyyyyy looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong textttttt\n\nShe had never observed his face more composed and she grabbed his hand and held it to her heart. It was resistless and dry. The outline of a skull was plain under his skin and the deep burned eye sockets seemed to lead into the dark tunnel where he had disappeared. She leaned closer and closer to his face, looking deep into them, trying to see how she had been cheated or what had cheated her, but she couldn't see anything. She shut her eyes and saw the pin point of light but so far away that she could not hold it steady in her mind. She felt as if she were blocked at the entrance of something. She sat staring with her eyes shut, into his eyes, and felt as if she had finally got to the beginning of something she couldn't begin, and she saw him moving farther and farther away, farther and farther into the darkness until he was the pin point of light.",
-    theme = "ascii", 
-    default_width="",
-    overflow="new_line"
-)
-panel2 = Panel(
-    inner_text = "This is a sample text!",
-    theme = "heavy",
-    default_width = "terminal"
+# panel1 = Panel(
+#     title=" Sample Title ",
+#     justify_title="center",
+#     title_padding=(0, 0),
+#     inner_text_padding=(1, 0, 2, 1),
+#     inner_text = "This is a sample line!\nThis is another sample line!\nThis is reallllllllyyyyyyyyyyyyyyyyyyy looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong textttttt\n\nShe had never observed his face more composed and she grabbed his hand and held it to her heart. It was resistless and dry. The outline of a skull was plain under his skin and the deep burned eye sockets seemed to lead into the dark tunnel where he had disappeared. She leaned closer and closer to his face, looking deep into them, trying to see how she had been cheated or what had cheated her, but she couldn't see anything. She shut her eyes and saw the pin point of light but so far away that she could not hold it steady in her mind. She felt as if she were blocked at the entrance of something. She sat staring with her eyes shut, into his eyes, and felt as if she had finally got to the beginning of something she couldn't begin, and she saw him moving farther and farther away, farther and farther into the darkness until he was the pin point of light.",
+#     theme = "ascii", 
+#     default_width="",
+#     overflow="new_line"
+# )
+# panel2 = Panel(
+#     inner_text = "This is a sample text!",
+#     theme = "heavy",
+#     default_width = "terminal"
+# )
+
+# panel3 = Panel()
+
+# panel3.set_title(
+#     title = " TITLE "
+# )
+# panel3.set_inner_text(
+#     inner_text = "This is a sample text!",
+#     default_width = "terminal",
+#     inner_text_padding= (0, 0, 2, 2)
+# )
+
+# print(panel1)
+# print(panel2)
+# print(panel3)
+
+table1 = Table(
+    title= "Star Wars Movies"
 )
 
-panel3 = Panel()
+table1.add_column(text= "Released", justify="center")
+table1.add_column(text= "Title", justify="center")
+table1.add_column(text= "Box Office", justify="center")
 
-panel3.set_title(
-    title = " TITLE "
-)
-panel3.set_inner_text(
-    inner_text = "This is a sample text!",
-    default_width = "terminal",
-    inner_text_padding= (0, 0, 2, 2)
-)
+table1.add_row("Dec 20, 2019 ", "Star Wars: The Rise of Skywalker", "$952,110,690")
+table1.add_row("May 25, 2018", "Solo: A Star Wars Story", "$393,151,347")
+table1.add_row("Dec 15, 2017", "Star Wars Ep. V111: The Last Jedi", "$1,332,539,889")
+table1.add_row("Dec 16, 2016", "Rogue One: A Star Wars Story", "$1,332,439,889")
 
-print(panel1)
-print(panel2)
-print(panel3)
+print(table1)
 
 
 
